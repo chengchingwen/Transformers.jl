ae78e09225cb0125a952c128f2941fa304c6802f
chengchingwen <adgjl5645@hotmail.com>
2019-01-11 02:14:41 +0800

    add loss without mask

--- src/basic/loss.jl
+++ src/basic/loss.jl
@@ -1,8 +1,8 @@
 "compute the kl divergence with mask where p is already the log(p)"
 logkldivergence(q::ThreeDimArray{T},
-       logp::ThreeDimArray{T},
-       mask) where T =
-           sum(reshape(sum(sum(q .* (log.(q .+ eps(q[1])) .- logp); dims=1) .* mask; dims=2), :) ./ reshape(sum(mask; dims=2), :))
+                logp::ThreeDimArray{T},
+                mask) where T =
+                    sum(reshape(sum(sum(q .* (log.(q .+ eps(q[1])) .- logp); dims=1) .* mask; dims=2), :) ./ reshape(sum(mask; dims=2), :))
 
 function logkldivergence(q, logp, mask)
     kld = (q .* (log.(q .+ eps(q[1])) .- logp)) #handle gpu broadcast error
@@ -18,4 +18,24 @@ logcrossentropy(q::ThreeDimArray{T},
 function logcrossentropy(q, logp, mask)
     ce = q .* logp
     sum(ce .* mask) / sum(mask)
+end
+
+"compute the kl divergence where p is already the log(p)"
+logkldivergence(q::ThreeDimArray{T},
+                logp::ThreeDimArray{T}) where T =
+                    sum(reshape(sum(sum(q .* (log.(q .+ eps(q[1])) .- logp); dims=1); dims=2), :) ./ size(q, 2))
+
+function logkldivergence(q, logp)
+    kld = (q .* (log.(q .+ eps(q[1])) .- logp)) #handle gpu broadcast error
+    sum(kld) / size(kld, 2)
+end
+
+"compute the cross entropy where p is already the log(p)"
+logcrossentropy(q::ThreeDimArray{T},
+                logp::ThreeDimArray{T}) where T =
+                    sum(reshape(sum(-sum(q .* logp; dims=1); dims=2), :) ./ size(q, 2))
+
+function logcrossentropy(q, logp)
+    ce = q .* logp
+    sum(ce) / size(ce, 2)
 end
