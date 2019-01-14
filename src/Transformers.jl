91f2a841a727aba3bb1021fafc27c9fdce02afe1
chengchingwen <adgjl5645@hotmail.com>
2019-01-14 23:24:49 +0800

    refactor: move fix function to outer module

--- src/Transformers.jl
+++ src/Transformers.jl
@@ -8,6 +8,28 @@ export dataset, datafile, get_batch, get_vocab
 
 export Gpt, load_gpt_pretrain, lmloss
 
+const ThreeDimArray{T} = AbstractArray{T, 3}
+const TwoDimArray{T} = AbstractArray{T, 2}
+
+device(x) = cpu(x)
+function use_gpu(use::Bool)
+    if use
+        @eval using CuArrays
+        @eval device(x) = gpu(x)
+    else
+        @eval device(x) = cpu(x)
+    end
+end
+
+#hack for gpu
+include("./fix/permutedims.jl")
+
+#implement batchmul for flux
+include("./fix/batchedmul.jl")
+
+#implement of gelu for cpu and gpu
+include("./fix/gelu.jl")
+
 include("./basic/Basic.jl")
 include("./datasets/Datasets.jl")
 
