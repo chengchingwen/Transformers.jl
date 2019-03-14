38f2cfb02dfb36d3946d46e3af9c78f9be352f1a
chengchingwen <adgjl5645@hotmail.com>
2019-03-14 23:48:03 +0800

    remove unused code

--- src/basic/extend3d.jl
+++ src/basic/extend3d.jl
@@ -30,6 +30,11 @@ macro toNd(ex, outref::Int=1)
          )
 end
 
+"""
+    Sequence(layers)
+
+just like Flux.Chain, but reshape input to 2d and reshape back when output.
+"""
 struct Sequence{T<:Tuple}
     models::T
     Sequence(xs...) = new{typeof(xs)}(xs)
@@ -38,51 +43,8 @@ end
 Flux.children(s::Sequence) = s.models
 Flux.mapchildren(f, s::Sequence) = Sequence(f.(s.models)...)
 
-
-# @generated function (s::Sequence{Ts})(x::AbstractArray{T, N}) where {Ts,T,N}
-#     ex = Expr(:block)
-#     fs = tuple(Ts.parameters...)
-#     arg = x
-#     n = N
-#     for f ∈ fs
-#         rt = Base.return_types(f, Tuple{arg})
-#         if rt <: AbstractArray
-#             n = rt.parameters[2]
-#         else
-#         end
-#
-#     end
-#
-#     ex
-# end
-
-# (s::Sequence)(x::ThreeDimArray) = @toNd s(x)
-# (s::Sequence)(x) = applychain(s.models, x)
-
 function (s::Sequence)(x)
     insize = size(x)
     y = applychain(s.models, reshape(x, insize[1], :))
     reshape(y, :, Base.tail(insize)...)
 end
-
-# #extend Flux op for 3-dims input
-# function (a::LayerNorm)(x::ThreeDimArray{T}) where T
-#     s = size(x)
-#     reshape(a(reshape(x, s[1], :)), s)
-# end
-
-# function (d::Dense)(x::ThreeDimArray{T}) where T
-#     s = size(x)
-#     reshape(d(reshape(x, s[1], :)), size(d.W, 1), s[2], s[3])
-# end
-
-# #avoid ambiguity
-# (a::Dense{<:Any,W})(x::ThreeDimArray{T}) where {T <: Union{Float32,Float64}, W <: AbstractArray{T}} =
-#   invoke(a, Tuple{ThreeDimArray}, x)
-
-# (a::Dense{<:Any,W})(x::ThreeDimArray{<:Real}) where {T <: Union{Float32,Float64}, W <: AbstractArray{T}} =
-#   a(T.(x))
-
-
-# logsoftmax3d(x) = logsoftmax(x)
-# logsoftmax3d(x::ThreeDimArray) = @toNd logsoftmax(x)
