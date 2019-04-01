024a0a8f7b9bf50cc0a2c9ffb05125952cdaf861
chengchingwen <adgjl5645@hotmail.com>
2019-04-02 01:35:54 +0800

    todevice function for handle input data

--- src/Transformers.jl
+++ src/Transformers.jl
@@ -9,10 +9,24 @@ export Stack, stack, @nntopo_str, show_stackfunc
 
 export dataset, datafile, get_batch, get_vocab
 
+export todevice
 export Gpt, load_gpt_pretrain, lmloss
 
 const Abstract3DTensor{T} = AbstractArray{T, 3}
 const Container{T} = Union{NTuple{N, T}, Vector{T}} where N
+
+"move data to device, when CuArrays is loaded, basically = `CuArrays.cu` except `AbstractArray{Int}` become `CuArray{Int}`"
+todevice(x) = x
+todevice(x, xs...) = (x, xs...)
+
+@init @require CuArrays="3a865a2d-5b23-5a0f-bc46-62713ec82fae" begin
+    import .CuArrays
+
+    "move data to device, basically = `CuArrays.cu` except `AbstractArray{Int}` become `CuArray{Int}`"
+    todevice(x, xs...) = (todevice(x), todevice.(xs)...)
+    todevice(x::AbstractArray{Int}) = CuArray(x)
+    todevice(x) = CuArrays.cu(x)
+end
 
 #implement batchmul for flux
 include("./fix/batchedmul.jl")
