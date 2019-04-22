f49b19418a2d4f3833f7a73430bd3ff0d27d300a
chengchingwen <adgjl5645@hotmail.com>
2019-04-23 00:44:54 +0800

    new topology implememtation

--- src/basic/stack/stack.jl
+++ src/basic/stack/stack.jl
@@ -6,10 +6,10 @@ using Flux
 
 like Flux.Chain, but you can use a NNTopo to define the order/structure of the function called.
 """
-struct Stack{T<:Tuple}
+struct Stack{T<:Tuple, FS}
     models::T
-    topo::NNTopo
-    Stack(topo, xs...) = new{typeof(xs)}(xs, topo)
+    topo::NNTopo{FS}
+    Stack(topo::NNTopo{FS}, xs...) where FS = new{typeof(xs), FS}(xs, topo)
 end
 
 Flux.children(s::Stack) = s.models
@@ -18,7 +18,7 @@ Flux.mapchildren(f, s::Stack) = Stack(s.topo, f.(s.models)...)
 (s::Stack)(xs...) = s.topo(s.models, xs...)
 
 "return a list of n model with give args"
-stack(n, modeltype::DataType, args...) = [modeltype(args...) for i = 1:n]
+stack(n, modeltype::DataType, args...; kwargs...) = [modeltype(args...; kwargs...) for i = 1:n]
 
 function Base.show(io::IO, s::Stack)
     print(io, "Stack(")
