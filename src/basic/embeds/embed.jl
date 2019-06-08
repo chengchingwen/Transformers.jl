0c480b07a4bb6e65d7fc4c4999c55a3d42d355d2
chengchingwen <adgjl5645@hotmail.com>
2019-06-09 00:08:30 +0800

    embed scale field

--- src/basic/embeds/embed.jl
+++ src/basic/embeds/embed.jl
@@ -4,6 +4,7 @@
 The Embedding Layer, `size` is the hidden size. `vocab_size` is the number of the vocabulary. Just a wrapper for embedding matrix.
 """
 struct Embed{F ,W <: AbstractArray{F}} <: AbstractEmbed{F}
+    scale::F
     embedding::W
 end
 
@@ -11,9 +12,16 @@ end
 
 Base.size(e::Embed, s...) = size(e.embedding, s...)
 
-Embed(size::Int, vocab_size::Int) = Embed(param(randn(Float32, size, vocab_size)))
+Embed(size::Int, vocab_size::Int; scale = one(Float32)) = Embed(Float32(scale), param(randn(Float32, size, vocab_size)))
 
-(e::Embed)(x::AbstractArray{Int}) = gather(e.embedding, x)
-(e::Embed{F})(x, scale) where {F} = e(x) .* convert(F, scale)
+function (e::Embed)(x::AbstractArray{Int})
+    if isone(e.scale)
+        gather(e.embedding, x)
+    else
+        e(x, e.scale)
+    end
+end
+
+(e::Embed{F})(x, scale) where {F} = gather(e.embedding, x) .* convert(F, scale)
 
 Base.show(io::IO, e::Embed) = print(io, "Embed($(size(e.embedding, 1)))")
