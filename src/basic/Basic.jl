ea08a75e759e17b62516a3b9c97782033bdac76d
chengchingwen <adgjl5645@hotmail.com>
2019-06-08 17:43:04 +0800

    add transformer model wrapper

--- src/basic/Basic.jl
+++ src/basic/Basic.jl
@@ -6,7 +6,7 @@ using Requires: @init
 
 using ..Transformers: Abstract3DTensor, Container, batchedmul
 
-export CompositeEmbeddings
+export CompositeEmbedding, TransformerModel
 export PositionEmbedding, Embed, getmask,
     Vocabulary, gather, encode, decode
 export OneHotArray, indices2onehot, onehot2indices,
