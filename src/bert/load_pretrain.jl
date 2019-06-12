685342c0bcc5e1b2653865eb3dbd1b6ef614aa6c
chengchingwen <adgjl5645@hotmail.com>
2019-06-13 01:46:18 +0800

    load bert model into transformer model

---
+++ src/bert/load_pretrain.jl
@@ -0,0 +1,19 @@
+isbson(s) = endswith(s, ".bson")
+istfbson(s) = endswith(s, ".tfbson")
+
+function load_bert_pretrain(path::AbstractString)
+  if istfbson(path)
+    @info "loading raw bert data: $path"
+    bson = BSON.load(path)
+    bson[:config], bson[:weights], bson[:vocab]
+  elseif isbson(path)
+    @info "loading bert model"
+    bson = BSON.load(path)
+    bson[:bert_model], bson[:vocab]
+  else
+    error("""
+          Unknown data type: Is it a bert model?
+          If it is the weight file get from google-bert, then run tfckpt2bson on that file beforehand.
+    """)
+  end
+end
