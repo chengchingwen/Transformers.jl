1ab072de68e2f8553a8ba242c3fcbb103ad1c1a2
chengchingwen <adgjl5645@hotmail.com>
2019-08-02 02:08:03 +0800

    update example with pretrain

--- example/FineTuneTransformerLM/1-model.jl
+++ example/FineTuneTransformerLM/1-model.jl
@@ -3,7 +3,7 @@ Reference: The origin code of GPT paper from openai (https://github.com/openai/f
 """
 
 using Flux
-using Flux: onecold, gradient, logitcrossentropy
+using Flux: onehotbatch, onecold, gradient, logitcrossentropy
 import Flux.Optimise: update!
 
 using BytePairEncoding
@@ -11,6 +11,9 @@ using BytePairEncoding
 using Transformers
 using Transformers.Basic
 using Transformers.GenerativePreTrain
+using Transformers.Pretrain
+
+ENV["DATADEPS_ALWAYS_ACCEPT"] = true
 
 include("./0-data.jl")
 
@@ -18,23 +21,24 @@ const startsym = "_start_"
 const delisym = "_deli_"
 const clfsym = "_clf_"
 const unksym = "<unk>"
-const anslabel = ["1", "2"]
-const anv = Vocabulary(anslabel, "1")
-gpt_model, bpe, vocab = load_gpt_pretrain(12;
-                                          startsym=startsym,
-                                          delisym=delisym,
-                                          clfsym=clfsym,
-                                          unksym=unksym)
+const labels = ("1", "2")
 
-const gpt = gpu(gpt_model)
+gpt_model, bpe, vocab, tokenizer = load_pretrain("GPT-OpenAIftlm";
+                                                 startsym=startsym,
+                                                 delisym=delisym,
+                                                 clfsym=clfsym,
+                                                 unksym=unksym)
+
+
+set_tokenizer(tokenizer)
+
+const gpt = gpu(Basic.set_classifier(gpt_model, Chain(Dense(768, 1), Dropout(0.1))))
 const embed = gpt.embed.embeddings.tok
-const clf = gpu(Dense(768, 1))
-
-const ansdrop = Dropout(0.1)
 
 function acc(p, y)
     pred = onecold(collect(p))
-    sum(pred .== collect(y)) / length(y)
+    label = onecold(collect(y))
+    sum(pred .== label) / length(label)
 end
 
 function loss(x1, x2, y, x1_mask, x2_mask, c1_index, c2_index)
@@ -47,22 +51,17 @@ function loss(x1, x2, y, x1_mask, x2_mask, c1_index, c2_index)
     c1 = gather(t1, c1_index)
     c2 = gather(t2, c2_index)
 
-    p1 = clf(c1)
-    p2 = clf(c2)
-    p = vcat(p1, p2)
-    p = ansdrop(p, 1)
+    p1 = gpt.classifier(c1)::typeof(c1)
+    p2 = gpt.classifier(c2)::typeof(c1)
+    p = vcat(p1, p2)::typeof(c1)
 
-    ##### turn onehot to real float array
-    yd = tofloat(Float32, onehot(anv, y))
-    #####
-
-    cl = logitcrossentropy(p, yd)
+    cl = logitcrossentropy(p, y)::typeof(lm)
     #unstable type will cause performance issue
     convert(Float32, 0.5) * lm + cl, p
 end
 
 const rocs = StoryCloze()
-const ps = params(gpt, clf)
+const ps = params(gpt)
 const opt = ADAM(6.25e-5)
 
 train!(args["epoch"])
