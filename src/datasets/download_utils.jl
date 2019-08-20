8350fa79c3f87e26d2cb91b213350904935cabf7
chengchingwen <adgjl5645@hotmail.com>
2019-08-20 19:13:17 +0800

    fix gdrive download failed due to using same cookie jar

--- src/datasets/download_utils.jl
+++ src/datasets/download_utils.jl
@@ -52,8 +52,9 @@ function find_gcode(ckj)
 end
 
 function download_gdrive(url, localdir)
-    rq = HTTP.request("HEAD", url; cookies=true)
-    ckj = HTTP.CookieRequest.default_cookiejar["drive.google.com"]
+    ckjar = copy(HTTP.CookieRequest.default_cookiejar)
+    rq = HTTP.request("HEAD", url; cookies=true, cookiejar=ckjar)
+    ckj = ckjar["drive.google.com"]
     gcode = find_gcode(ckj)
     @assert gcode !== nothing
 
@@ -63,12 +64,11 @@ function download_gdrive(url, localdir)
     format_bytes_per_second(x) = format_bytes(x) * "/s"
 
     local filepath
-
-    newurl = unshortlink("$url&confirm=$gcode"; cookies=true)
+    newurl = unshortlink("$url&confirm=$gcode"; cookies=true, cookiejar=ckjar)
 
 
     #part of codes are from https://github.com/JuliaWeb/HTTP.jl/blob/master/src/download.jl
-    HTTP.open("GET", newurl, ["Range"=>"bytes=0-"]; cookies=true) do stream
+    HTTP.open("GET", newurl, ["Range"=>"bytes=0-"]; cookies=true, cookiejar=ckjar) do stream
         resp = HTTP.startread(stream)
         hcd = HTTP.header(resp, "Content-Disposition")
         m = match(r"filename=\\\"(.*)\\\"", hcd)
@@ -85,6 +85,7 @@ function download_gdrive(url, localdir)
         downloaded_bytes = 0
         start_time = now()
         prev_time = now()
+        period = DataDeps.progress_update_period()
 
         function report_callback()
             prev_time = now()
@@ -111,8 +112,10 @@ function download_gdrive(url, localdir)
         Base.open(filepath, "w") do fh
             while(!eof(stream))
                 downloaded_bytes += write(fh, readavailable(stream))
-                if now() - prev_time > Millisecond(1000*2)
+                if !isinf(period)
+                  if now() - prev_time > Millisecond(1000*period)
                     report_callback()
+                  end
                 end
             end
         end
