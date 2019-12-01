6bdbe2d3e72b176f76dc9a6ec6c86c40b95d32bb
chengchingwen <adgjl5645@hotmail.com>
2019-12-01 14:01:02 +0800

    optimize stack

--- src/stacks/stack.jl
+++ src/stacks/stack.jl
@@ -1,5 +1,5 @@
 using Flux
-using MacroTools: @forward
+using MacroTools: @forward, postwalk
 
 """
     Stack(topo::NNTopo, layers...)
@@ -14,12 +14,28 @@ end
 
 Flux.functor(s::Stack) = s.models, m -> Stack(s.topo, m...)
 
-(s::Stack)(xs...) = s.topo(s.models, xs...)
+@generated function (s::Stack{TP, FS})(xs...) where {TP, FS}
+    _code = nntopo_impl(FS)
+    n = fieldcount(TP)
+    ms = [Symbol(:__model, i, :__) for i = 1:n]
+    head = Expr(:(=), Expr(:tuple, ms...), :(s.models))
+    pushfirst!(_code.args, head)
+    code = postwalk(_code) do x
+        if x isa Expr && x.head === :ref && x.args[1] === :model
+            i = x.args[2]
+            y = :($(ms[i]))
+            return y
+        else
+            x
+        end
+    end
+    return code
+end
 
 @forward Stack.models Base.getindex, Base.length
 
 "return a list of n model with give args"
-stack(n, modeltype::DataType, args...; kwargs...) = [modeltype(args...; kwargs...) for i = 1:n]
+stack(n, modeltype::Type{T}, args...; kwargs...) where T = [modeltype(args...; kwargs...) for i = 1:n]
 
 function Base.show(io::IO, s::Stack)
     print(io, "Stack(")
