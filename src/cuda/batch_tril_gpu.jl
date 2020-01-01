0c4735d61c0e47d1866178215e0e4af9c8b8b29b
chengchingwen <adgjl5645@hotmail.com>
2020-01-01 21:23:59 +0800

    add batch triu

--- src/cuda/batch_tril_gpu.jl
+++ src/cuda/batch_tril_gpu.jl
@@ -1,4 +1,4 @@
-using LinearAlgebra: tril!
+using LinearAlgebra: tril!, triu!
 function batched_tril!(A::CuArray{T, N}, d) where {T, N}
   if N < 2
     error("MethodError: no method matching tril!(::Array{Float64,1})")
@@ -27,6 +27,38 @@ function batched_tril!(A::CuArray{T, N}, d) where {T, N}
     threads = (thread_x, thread_y)
     blocks = ceil.(Int, (l, bs) ./ threads)
     @cuda threads=threads blocks=blocks batch_tril_kernel!(A, d)
+    return A
+  end
+end
+
+function batched_triu!(A::CuArray{T, N}, d) where {T, N}
+  if N < 2
+    error("MethodError: no method matching triu!(::Array{Float64,1})")
+  elseif N == 2
+    return tril!(x, d)
+  else
+    s = size(A)
+    m, n = s[1], s[2]
+    l = m*n
+    bs = Int(length(A) // l)
+    function batch_triu_kernel!(_A, _d)
+      li = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+      b = (blockIdx().y - 1) * blockDim().y + threadIdx().y
+      @inbounds if 0 < li <= l && b <= bs
+        id = Tuple(CartesianIndices(_A)[Base._to_linear_index(_A, mod1(li, m), fld1(li, m), b)])
+        i, j = id
+        if j < i + _d
+          _A[id...] = 0
+        end
+      end
+      return nothing
+    end
+    max_threads = 256
+    thread_x = min(max_threads, l)
+    thread_y = min(max_threads ÷ thread_x, bs)
+    threads = (thread_x, thread_y)
+    blocks = ceil.(Int, (l, bs) ./ threads)
+    @cuda threads=threads blocks=blocks batch_triu_kernel!(A, d)
     return A
   end
 end
--- src/fix/batched_tril.jl
+++ src/fix/batched_tril.jl
@@ -1,4 +1,4 @@
-using LinearAlgebra: tril!
+using LinearAlgebra: tril!, triu!
 
 function batched_tril!(x::A, d) where {T, N, A <: AbstractArray{T, N}}
   if N < 2
@@ -13,6 +13,24 @@ function batched_tril!(x::A, d) where {T, N, A <: AbstractArray{T, N}}
     Wt = Core.apply_type(A.name.wrapper, T, 2)
     Threads.@threads for i = 1:len
       tril!(Base.unsafe_wrap(Wt, Base.pointer(x, (ms * (i - 1) + 1)), m), d)
+    end
+    return x
+  end
+end
+
+function batched_triu!(x::A, d) where {T, N, A <: AbstractArray{T, N}}
+  if N < 2
+    error("MethodError: no method matching triu!(::Array{Float64,1})")
+  elseif N == 2
+    return triu!(x, d)
+  else
+    s = size(x)
+    m = (s[1], s[2])
+    ms = s[1] * s[2]
+    len = Int(length(x) // ms)
+    Wt = Core.apply_type(A.name.wrapper, T, 2)
+    Threads.@threads for i = 1:len
+      triu!(Base.unsafe_wrap(Wt, Base.pointer(x, (ms * (i - 1) + 1)), m), d)
     end
     return x
   end
