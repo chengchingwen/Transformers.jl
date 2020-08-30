24f2203f160cd16398d489557821e7ba12317a20
chengchingwen <adgjl5645@hotmail.com>
2020-08-30 18:54:48 +0800

    macro for loading pretrain

--- src/huggingface/HuggingFace.jl
+++ src/huggingface/HuggingFace.jl
@@ -5,7 +5,10 @@ using ..Transformers
 export get_or_download_hgf_file,
   get_or_download_hgf_config,
   get_or_download_hgf_weight,
+  @hgf_str,
   load_config,
+  load_model,
+  load_model!,
   load_state_dict,
   load_state
 
@@ -14,5 +17,30 @@ include("./weight.jl")
 include("./configs/config.jl")
 include("./models/models.jl")
 
+macro hgf_str(name)
+  :(load_hgf_pretrained($(esc(name))))
+end
+
+get_model_type(model, task) = error("Model $model doesn't support this kind of task: $task")
+
+function load_hgf_pretrained(name)
+  model_name, item = rsplit(name, ':'; limit=2)
+  get_or_download_hgf_config(model_name)
+  cfg = load_config(model_name)
+
+  item == "config" &&
+    return cfg
+
+  item == "tokenizer" &&
+    error("tokenizer not support yet.")
+
+  model_type = cfg.model_type
+  model_cons = get_model_type((Val ∘ Symbol ∘ lowercase)(model_type), (Val ∘ Symbol ∘ lowercase)(item))
+
+  get_or_download_hgf_weight(model_name)
+  model = load_model(model_cons, model_name; config=cfg)
+
+  return model
+end
 
 end
