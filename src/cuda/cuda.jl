cb1ba945258584536fae2572359a595f802ab235
chengchingwen <adgjl5645@hotmail.com>
2022-05-22 01:38:56 +0800

    hack for todevice type stability

--- src/cuda/cuda.jl
+++ src/cuda/cuda.jl
@@ -2,8 +2,19 @@ using CUDA
 
 import CUDA: CuArray
 
+@generated function togpudevice(x::T) where {T <: AbstractArray}
+    # https://github.com/FluxML/Flux.jl/blob/master/src/functor.jl#L89
+    _R = Core.Compiler.return_type(CUDA.cu, Tuple{x})
+    R = if _R isa Union
+        T == _R.a ? _R.b : _R.a
+    else
+        _R
+    end
+    return :(gpu(x)::$R)
+end
+
 togpudevice(x) = gpu(x)
-togpudevice(x, xs...) = (todevice(x), map(todevice, xs)...)
+togpudevice(x, xs...) = (togpudevice(x), map(togpudevice, xs)...)
 
 include("./statistic.jl")
 include("./batch_gemm_gpu.jl")
