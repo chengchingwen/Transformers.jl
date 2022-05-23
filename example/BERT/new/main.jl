551c8dde9ad13fd3ac49d5edf1ec98af6e253990
chengchingwen <adgjl5645@hotmail.com>
2022-05-23 08:02:44 +0800

    bert cola example

---
+++ example/BERT/new/main.jl
@@ -0,0 +1,28 @@
+using ArgParse
+using Transformers
+
+ENV["DATADEPS_ALWAYS_ACCEPT"] = true
+
+function parse_commandline()
+    s = ArgParseSettings()
+
+    @add_arg_table s begin
+        "--gpu", "-g"
+            help = "use gpu"
+            action = :store_true
+        "task"
+            help = "task name"
+            required = true
+            range_tester = x-> x ∈ ["cola", "mnli", "mrpc"]
+    end
+
+    return parse_args(ARGS, s)
+end
+
+const args = parse_commandline()
+
+enable_gpu(args["gpu"])
+
+const task = args["task"]
+
+include(joinpath(@__DIR__, task, "train.jl"))
