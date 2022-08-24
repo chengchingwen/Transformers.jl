1a7c160fdddd768996a26998f83b2806c2152dbc
chengchingwen <adgjl5645@hotmail.com>
2022-08-24 16:29:44 +0800

    update env; move bert text encode utils to basic; add concat; allow with first head tail take sep sym

--- src/bert/BidirectionalEncoder.jl
+++ src/bert/BidirectionalEncoder.jl
@@ -8,6 +8,7 @@ using BSON
 using TextEncodeBase
 
 using ..Basic
+using ..Basic: with_firsthead_tail, segment_and_concat
 using ..Pretrain: isbson, iszip, istfbson, zipname, zipfile, findfile
 export Bert, load_bert_pretrain, bert_pretrain_task, masklmloss
 export BertTextEncoder, bert_cased_tokenizer, bert_uncased_tokenizer
@@ -17,7 +18,6 @@ include("tfckpt2bson.jl")
 include("load_pretrain.jl")
 include("tokenizer.jl")
 include("wordpiece.jl")
-include("utils.jl")
 include("textencoder.jl")
 include("pretrain_helper.jl")
 
