f2b30d0d238e0d68f03cdfa7f3c67499587af2ac
chengchingwen <adgjl5645@hotmail.com>
2022-12-28 04:41:35 +0800

    update huggingface task validate code

--- example/HuggingFaceValidation/task_head.jl
+++ example/HuggingFaceValidation/task_head.jl
@@ -3,7 +3,7 @@ function test_task_head(name, n; max_error = 1e-1, mean_error = 1e-2)
     @info "Validate $name task head"
     @testset "Task Head" begin
         @info "Found task type in confingure file" tasks = config.architectures
-        model_type = config.model_type |> Symbol |> Val
+        model_type = config.model_type |> Symbol
 
         for task_type in config.architectures
             task_type = task_type_map(task_type)
@@ -16,9 +16,8 @@ function test_task_head(name, n; max_error = 1e-1, mean_error = 1e-2)
 
             @info "Loading model with $task_type head in Julia"
             model = @tryrun begin
-                jl_task_type = chop(task_type, head=length(config.model_type), tail=0) |> lowercase |> Symbol |> Val
-                jl_model_cons = HuggingFace.get_model_type(model_type, jl_task_type)
-                HuggingFace.load_model(jl_model_cons, name; config)
+                jl_task_type = chop(task_type, head=length(config.model_type), tail=0) |> lowercase |> Symbol
+                HuggingFace.load_model(model_type, name, jl_task_type; config)
             end "Failed to load the model in Julia"
             @info "Julia model loaded successfully"
 
@@ -32,21 +31,21 @@ function test_task_head(name, n; max_error = 1e-1, mean_error = 1e-2)
                     indices2 = rand(1:vocab_size, len2)
                     pyindices2 = torch.tensor(indices2 .- 1).reshape(1, len2)
                     py_states = hgf_model(input_ids = pyindices, decoder_input_ids = pyindices2)
-                    jl_states = model(indices, indices2)
+                    jl_states = model((encoder_input = (token = indices,), decoder_input = (token = indices2,)))
                 else
                     py_states = hgf_model(pyindices)
-                    jl_states = model(reshape(indices, len, 1))
+                    jl_states = model((token = reshape(indices, len, 1),))
                 end
 
-                if haskey(jl_states, :logits)
+                if haskey(py_states, "logits")
                     py_result = rowmaj2colmaj(py_states["logits"].detach().numpy())
-                    jl_result = jl_states.logits
-                elseif haskey(jl_states, :prediction_logits)
+                    jl_result = jl_states.logit
+                elseif haskey(py_states, "prediction_logits")
                     py_result = rowmaj2colmaj(py_states["prediction_logits"].detach().numpy())
-                    jl_result = jl_states.prediction_logits
-                elseif haskey(jl_states, :start_logits)
+                    jl_result = jl_states.logit
+                elseif haskey(py_states, "start_logits")
                     py_result = rowmaj2colmaj(py_states["start_logits"].detach().numpy())
-                    jl_result = jl_states.start_logits
+                    jl_result = jl_states.start_logit
                 end
                 jl_result = reshape(jl_result, Val(ndims(py_result)))
                 diff = (py_result .- jl_result) .^ 2
