3d2638874dfbd2d6467937022cf495b420a19f2a
chengchingwen <adgjl5645@hotmail.com>
2023-03-08 18:07:44 +0800

    update huggingface validation code for whole model

--- example/HuggingFaceValidation/whole_model.jl
+++ example/HuggingFaceValidation/whole_model.jl
@@ -1,100 +1,112 @@
 function test_whole_model(name, corpus; max_error = 1e-1, mean_error = 1e-2)
-    global torch, hgf_trf, config, vocab_size
+    global torch, hgf_trf, config, pyconfig, vocab_size
     global error_samples = []
     @info "Validate $name whole model with corpus $corpus"
     @testset "Whole model" begin
-        model_type = config.model_type |> Symbol |> Val
+        model_type = config.model_type |> Symbol
         task_type = first(config.architectures)
         isfile(corpus) || error("corpus file $corpus do not exist.")
 
         @info "Loading tokenizer in Python"
         global hgf_tkr = @tryrun begin
-            hgf_trf.AutoTokenizer.from_pretrained(name)
+            hgf_trf.AutoTokenizer.from_pretrained(name, config = pyconfig)
         end "Failed to load the tokenizer in Python"
         @info "Python tokenizer loaded successfully"
 
         @info "Loading tokenizer in Julia"
         global tkr = @tryrun begin
-            HuggingFace.load_hgf_pretrained("$name:tokenizer")
+            HuggingFace.load_tokenizer(name; config)
         end "Failed to load the tokenizer in Julia"
         @info "Julia tokenizer loaded successfully"
 
         @info "Loading model with $task_type head in Python"
         global hgf_model = @tryrun begin
             pyauto = hgf_trf[task_type]
-            pyauto.from_pretrained(name)
+            pyauto.from_pretrained(name, config = pyconfig)
         end "Failed to load model in Python"
         @info "Python model loaded successfully"
 
         @info "Loading model with $task_type head in Julia"
         global model = @tryrun begin
-            jl_task_type = chop(task_type, head=length(config.model_type), tail=0) |> lowercase |> Symbol |> Val
-            jl_model_cons = HuggingFace.get_model_type(model_type, jl_task_type)
-            HuggingFace.load_model(jl_model_cons, name; config)
+            jl_task_type = chop(task_type, head=length(config.model_type), tail=0) |> lowercase |> Symbol
+            HuggingFace.load_model(model_type, name, jl_task_type; config)
         end "Failed to load the model in Julia"
         @info "Julia model loaded successfully"
 
         @info "Testing: Whole model"
         fd = nothing
+        sum_hidden_avg_err = 0.0
+        sum_hidden_max_err = 0.0
+        sum_avg_err = 0.0
+        sum_max_err = 0.0
+        len = 0
         try
             fd = open(corpus)
             for line in eachline(fd)
                 isempty(line) && continue
+                len += 1
                 jl_tokens = TextEncodeBase.getvalue.(TextEncodeBase.tokenize(tkr, line))
                 py_tokens = hgf_tkr.tokenize(line)
                 @test jl_tokens == py_tokens
-                jl_indices = collect(reinterpret(Int32, encode(tkr, line).input.tok))
-                py_indices = collect(hgf_tkr(line)["input_ids"]) .+ 1
+                jl_indices = collect(reinterpret(Int32, encode(tkr, line).token))
+                _py_indices = collect(hgf_tkr(line)["input_ids"])
+                py_indices = _py_indices .+ 1
                 @test jl_indices == py_indices
 
-                py_input = torch.tensor(collect(hgf_tkr(line)["input_ids"])).reshape(1, length(py_indices))
-                jl_input = encode(tkr, [line]).input.tok
-                py_states = hgf_model(py_input)
-                jl_states = model(jl_input)
-
-                if haskey(jl_states, :start_logits)
-                    py_result = rowmaj2colmaj(py_states["start_logits"].detach().numpy())
-                    jl_result = jl_states.start_logits
-                    py_result2 = rowmaj2colmaj(py_states["end_logits"].detach().numpy())
-                    jl_result2 = jl_states.end_logits
-
-                    jl_result = reshape(jl_result, Val(ndims(py_result)))
-                    jl_result2 = reshape(jl_result2, Val(ndims(py_result2)))
-
-                    jl_pred = Flux.onecold(jl_result)
-                    py_pred = Flux.onecold(py_result)
-                    jl_pred2 = Flux.onecold(jl_result2)
-                    py_pred2 = Flux.onecold(py_result2)
-
-                    @test jl_pred == py_pred
-                    @test jl_pred2 == py_pred2
-
-                    if jl_pred != py_pred || jl_pred2 != py_pred2
-                        push!(error_samples, (line, (jl_input, jl_pred, jl_pred2), (py_input, py_pred, py_pred2)))
-                    end
+                if HuggingFace.is_seq2seq(model)
+                    py_input = torch.tensor(_py_indices).reshape(1, length(py_indices))
+                    jl_input = encode(tkr, line, line)
+                    py_states = hgf_model(input_ids = py_input, decoder_input_ids = py_input,
+                                          output_hidden_states = true)
+                    jl_states = model(jl_input)
                 else
-                    if haskey(jl_states, :logits)
-                        py_result = rowmaj2colmaj(py_states["logits"].detach().numpy())
-                        jl_result = jl_states.logits
-                    elseif haskey(jl_states, :prediction_logits)
-                        py_result = rowmaj2colmaj(py_states["prediction_logits"].detach().numpy())
-                        jl_result = jl_states.prediction_logits
-                    end
-                    jl_result = reshape(jl_result, Val(ndims(py_result)))
+                    py_input = torch.tensor(_py_indices).reshape(1, length(py_indices))
+                    jl_input = encode(tkr, line)
+                    py_states = hgf_model(py_input, output_hidden_states = true)
+                    jl_states = model(jl_input)
+                end
+                py_hidden = rowmaj2colmaj(py_states["hidden_states"][end].detach().numpy())
+                jl_hidden = jl_states.hidden_state
+                diff_hidden = (py_hidden .- jl_hidden) .^ 2
+                avg_hidden_err = mean(diff_hidden)
+                max_hidden_err = maximum(diff_hidden)
+                sum_hidden_avg_err += avg_hidden_err
+                sum_hidden_max_err = max(max_hidden_err, sum_hidden_max_err)
+                @test avg_hidden_err < mean_error
+                @test max_hidden_err < max_error
 
-                    jl_pred = Flux.onecold(jl_result)
-                    py_pred = Flux.onecold(py_result)
-                    @test jl_pred == py_pred
+                if haskey(py_states, "logits")
+                    py_result = rowmaj2colmaj(py_states["logits"].detach().numpy())
+                    jl_result = jl_states.logit
+                elseif haskey(py_states, "prediction_logits")
+                    py_result = rowmaj2colmaj(py_states["prediction_logits"].detach().numpy())
+                    jl_result = jl_states.logit
+                elseif haskey(py_states, "start_logits")
+                    py_result = rowmaj2colmaj(py_states["start_logits"].detach().numpy())
+                    jl_result = jl_states.start_logit
+                end
+                jl_result = reshape(jl_result, Val(ndims(py_result)))
+                diff = (py_result .- jl_result) .^ 2
+                avg_err = mean(diff)
+                max_err = maximum(diff)
+                sum_avg_err += avg_err
+                sum_max_err = max(max_err, sum_max_err)
+                @test avg_err < mean_error
+                @test max_err < max_error
 
-                    if jl_pred != py_pred
-                        push!(error_samples, (line, (jl_input, jl_pred), (py_input, py_pred)))
-                    end
+                jl_pred = Flux.onecold(jl_result)
+                py_pred = Flux.onecold(py_result)
+                @test jl_pred == py_pred
+                if jl_pred != py_pred
+                    push!(error_samples, (line, (jl_input, jl_pred), (py_input, py_pred)))
                 end
             end
         catch e
             isnothing(fd) || close(fd)
             rethrow(e)
         end
+        @info "average hidden_state error" max = sum_hidden_max_err mean = sum_hidden_avg_err / len
+        @info "average logit error" max = sum_max_err mean = sum_avg_err / len
     end
 end
 
