6871a97b5b467b0def9a1d596e7695cad78c4de6
Devesh Jawla <devesh.jawla@gmail.com>
2024-10-25 13:38:54 +0100

    Added Distilbert and Roberta Examples

---
+++ example/Roberta_FillMask/fill_mask.jl
@@ -0,0 +1,33 @@
+using Transformers
+using FuncPipelines, TextEncodeBase
+using TextEncodeBase: nested2batch, nestedcall  
+using Flux, StatsBase
+
+conf = Transformers.HuggingFace.load_config("distilbert/distilroberta-base")
+
+tkr = Transformers.HuggingFace.load_tokenizer("distilbert/distilroberta-base")
+model = Transformers.HuggingFace.hgf"distilbert/distilroberta-base:formaskedlm"
+
+tkr.tokenizer.tokenization
+
+tkr.tokenizer.tokenization.patterns
+
+new_tkr = Transformers.TextEncoders.BertTextEncoder(tkr) do e
+	e.process[1:5] |> Pipeline{:masked_position}(nested2batch ∘ nestedcall(isequal("<mask>")), :token) |> e.process[6:end-1] |> PipeGet{(:token, :segment, :attention_mask, :masked_position)}()
+end
+
+query = "Paris is the<mask> of France."
+
+input = Transformers.TextEncoders.encode(new_tkr, query)
+
+input_ids = input.masked_position
+
+Transformers.TextEncoders.decode(new_tkr, input.token)
+
+model_output = model(input)
+mask_logits = model_output.logit[:, :, 1]
+
+mask_probabilities = softmax(mask_logits, dims=1)
+predicted_token_id = map(argmax, eachcol(mask_probabilities))
+
+predicted_token = Transformers.TextEncoders.decode(new_tkr, predicted_token_id)#[input_ids]
